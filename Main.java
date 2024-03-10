d804d94c7274c0468cc0c9fd3bc17a6add6a782d
Sainath <sainathdora4@gmail.com>
2024-03-10 09:45:24 +0530

    Storing objects of different dtypes [1, 2, sai]

--- Main.java
+++ Main.java
@@ -4,20 +4,23 @@ import java.util.Scanner;
 public class Main {
     
     public static void main(String[] args) {
+        // Storing objects of different data types
+        // [1, 2, "sainath"];
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
         String[] input_arr = input.split(" ");
-        ArrayList <Integer> arr = new ArrayList<>();
+        ArrayList <Object> arr = new ArrayList<>();
         for(var i:input_arr){
             try{
-                arr.add(Integer.parseInt(i));
+                arr.add(i);
             }catch(Exception err){
                 System.out.println("err.getMessage(): "+err.getMessage());
             }
         }
-      for(var i :input.getClass().getMethods()){
-        System.out.println(i);
-      }
+        for(var i:arr){
+            System.out.println(i);
+        }
+   
 }
     
 }
